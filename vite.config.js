d01b6a2cc8787e84ac434f7465c0d5fdad9dcbf4
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-02-28 14:22:56 +0000

    vite config adjusted

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Specify the output directory, change to 'dist' if preferred
+    cssCodeSplit: true,  // Ensures CSS files are split correctly
+    rollupOptions: {
+      input: '/src/main.jsx',  // Adjust this based on your entry point, default could be index.jsx or index.tsx
+    }
+  },
 })
