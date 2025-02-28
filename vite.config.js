2265e17292b30f05a40540d4838f54d6226c7662
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-02-28 17:51:27 +0000

    modified assets and css path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/build/',
   build: {
     outDir: 'build',  // Specify the output directory, change to 'dist' if preferred
     cssCodeSplit: true,  // Ensures CSS files are split correctly
