ef7b684fbb5341d6f4903e2c620f0ec632c15399
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-03-01 08:23:32 +0000

    changed build route to dist

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/build/',
+  publicDir: 'public',
   build: {
-    outDir: 'build',  // Specify the output directory, change to 'dist' if preferred
+    outDir: 'dist/build',  // Specify the output directory, change to 'dist' if preferred
     cssCodeSplit: true,  // Ensures CSS files are split correctly
     rollupOptions: {
       input: 'index.html',  // Make sure Vite uses the root index.html file
