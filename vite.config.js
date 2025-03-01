a6496eaeffb56e3b229e8f36b1f0d69c4fab8d38
Henry Tottle <henry@Henrys-MacBook-Air.local>
2025-03-01 20:32:25 +0000

    got it working on plesk now trying aws

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   publicDir: 'public',
   build: {
     outDir: 'dist',  // Specify the output directory, change to 'dist' if preferred
